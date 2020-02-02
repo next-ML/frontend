b415f9b34619ba8422028c6fb55986c0a5905cdc
Sword-holder <iamthexzy@gmail.com>
2020-02-02 13:57:16 +0800

    Add training config module.

---
+++ src/components/trainingConfig.vue
@@ -0,0 +1,69 @@
+<template>
+  <el-card shadow="never">
+    <!-- 这个form并没有实际用途 -->
+    <el-form ref="form" label-width="80px">
+      <el-row style="height: 0px;">
+        <el-col :span="3">
+          <el-form-item label="推荐配置">
+            <el-switch v-model="recommendedConfig"></el-switch>
+          </el-form-item>
+        </el-col>
+        <el-col :span="5" :offset="1">
+          <el-form-item label="搜索时间">
+            <el-time-select style="width: 70%;"
+              v-model="trainingTime"
+              :picker-options="{
+                start: '00:30',
+                step: '00:30',
+                end: '100:00'
+              }"
+              placeholder="选择时间">
+            </el-time-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="5" :offset="0">
+          <el-form-item label="尝试次数">
+            <el-input-number size="medium" style="width: 70%;" controls-position="right"
+                            v-model="tryTimes" 
+                            :min="1" 
+                            label="尝试次数">
+            </el-input-number>
+          </el-form-item>
+        </el-col>
+        <el-col :span="5">
+          <el-form-item label="节点数量">
+            <el-input-number size="medium" style="width: 70%;" controls-position="right"
+                            v-model="nodeNum" 
+                            :min="1" 
+                            :max="100" 
+                            label="节点数量">
+            </el-input-number>
+          </el-form-item>
+        </el-col>
+        <el-col :span="5">
+          <el-form-item>
+            <el-button style="width: 60%" type="primary" @click="startTraining">开始训练</el-button>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: "TrainingConfig",
+  data() {
+    return {
+      tryTimes: 1,
+      nodeNum: 1,
+      trainingTime: "", // 这个时间是一个string
+      recommendedConfig: false,
+    }
+  },
+  methods: {
+    startTraining() {
+    }
+  }
+}
+</script>
--- src/pages/modelTraining.vue
+++ src/pages/modelTraining.vue
@@ -1,22 +1,106 @@
 <template>
-<div>
-  <el-image
-      style="width: 80%; height: 80%"
-      :src="underConstructionImg"
-      :fit="fit"></el-image>
-</div>
+  <div>
+    <el-card class="box-card" shadow="hover">
+      <chart style="width: 100%; height:calc(100vh - 260px);" 
+            :options="chartOptionsHistoryLine"></chart>
+      <el-row>
+        <el-progress :percentage="percentage" :color="customColors"></el-progress>
+      </el-row>
+    </el-card>
+    <training-config></training-config>
+  </div>
 </template>
 
 <script>
+import 'echarts/lib/chart/line';
+import 'echarts/lib/chart/bar';
+import 'echarts/lib/component/markLine'
+import 'echarts/lib/component/legend'
+import 'echarts/lib/component/title'
+import 'echarts/lib/component/markPoint'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/toolbox'
+
+import TrainingConfig from "~/components/trainingConfig"
+
 export default {
+  name: "TrainingResult", 
+  components: {
+    "training-config": TrainingConfig
+  },
   data() {
     return {
-      underConstructionImg: require("~/assets/underConstruction.jpg")
+      chartOptionsHistoryLine: {
+        title: {
+          text: '训练历史',
+          subtext: '记录了每一次的训练结果'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data:['均方误差']
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            dataZoom: {
+              yAxisIndex: 'none'
+            },
+            dataView: {readOnly: false},
+            magicType: {type: ['line', 'bar']},
+            restore: {},
+            saveAsImage: {}
+          }
+        },
+        xAxis:  {
+          type: 'category',
+          boundaryGap: false,
+          data: ['1','2','3','4','5','6','7']
+        },
+        yAxis: {
+          type: 'value',
+          axisLabel: {
+            formatter: '{value}'
+          }
+        },
+        series: [
+          {
+            name:'均方误差',
+            type:'line',
+            data:[51, 31, 65, 63, 42, 23, 55],
+            markPoint: {
+              data: [
+                {type: 'min', name: '最小值'}
+              ]
+            },
+            markLine: {
+              data: [
+                {
+                  type: 'average', 
+                  name: '平均值',
+                },
+              ]
+            }
+          },
+        ]
+      },
+      // 进度条
+      percentage: 50,
+      customColor: '#409eff',
+      customColors: [
+        {color: '#f56c6c', percentage: 20},
+        {color: '#e6a23c', percentage: 40},
+        {color: '#5cb87a', percentage: 60},
+        {color: '#1989fa', percentage: 80},
+        {color: '#6f7ad3', percentage: 100}
+      ]
+    }
+  },
+  methods: {
+    format(percentage) {
+      return percentage === 100 ? '完成' : `${percentage}%`;
     }
   }
 }
 </script>
-
-<style scoped>
-  
-</style>
